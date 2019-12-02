d8c0e5b0fe5d4f7251e83810fe91e66b342a13d3
guqianfeng <guqianfeng@angelalign.com>
2019-12-02 12:20:55 +0800

    简单请求和预检请求的学习

--- Day16-30/day-22/demo4000/index.js
+++ Day16-30/day-22/demo4000/index.js
@@ -10,11 +10,22 @@ app.use(static(__dirname + "/static"));
 app.use(koaBody());
 app.use(router.routes());
 
+router.options("/*", ctx => {
+    //可以跨域的地址
+    ctx.set("Access-Control-Allow-Origin", "http://localhost:3000");
+    //设置请求头的信息，这里用content-type做演示，可以追加其他的属性
+    ctx.set("Access-Control-Allow-Headers", "content-type");
+    ctx.body = "";
+})
+
 router.get("/", ctx => {
     ctx.body = "hello 4000";
 })
 
 router.get("/getData", ctx => {
+    ctx.set("Access-Control-Allow-Origin", "http://localhost:3000");
+    //设置响应头的信息，前端可以拿到，这里用date做演示，也可以追加其他的属性
+    ctx.set("Access-Control-Expose-Headers", "Date")
     ctx.body = {
         status: 1,
         msg: "4000 msg"
